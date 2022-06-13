d2341d195e15dc253026e103bd267ed669702eb3
Maria Eduarda Couto <mariaeduardacoutt@gmail.com>
2022-06-13 19:38:41 +0200

    Design enhancement

--- app/src/main/java/com/app/riddle/scenes/login/LoginActivity.java
+++ app/src/main/java/com/app/riddle/scenes/login/LoginActivity.java
@@ -3,6 +3,7 @@ package com.app.riddle.scenes.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,7 +17,9 @@ import com.app.riddle.scenes.feed.FeedActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 
+
 public class LoginActivity extends AppCompatActivity {
+
 
     private Button btn_login;
     private EditText input_email;
